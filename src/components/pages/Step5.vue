970ec64c72dca108e41d57ef9ea548c6bd1ad006
Antoine Falisse <antoinefalisse@gmail.com>
2023-10-09 09:27:02 -0700

    temporarily comment analysis

--- src/components/pages/Step5.vue
+++ src/components/pages/Step5.vue
@@ -81,7 +81,7 @@
                             </v-dialog>
 
                           </v-list-item>
-                          <v-list-item link v-if="!t.trashed && t.name !== 'neutral'">
+                          <!-- <v-list-item link v-if="!t.trashed && t.name !== 'neutral'">
                             <v-dialog
                                     v-model="showAnalysisDialog"
                                     v-click-outside="clickOutsideDialogTrialHideMenu"
@@ -159,7 +159,7 @@
                                 </v-card>
                             </v-dialog>
 
-                          </v-list-item>
+                          </v-list-item> -->
                           <v-list-item link v-if="!t.trashed">
                             <v-dialog
                                     v-model="remove_dialog"
