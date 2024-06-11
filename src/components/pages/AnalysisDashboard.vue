d8d8944a5449b93eb529af8719be03b87dc4712e
carmichaelong <carmichael.ong@gmail.com>
2024-06-11 16:05:33 -0700

    add squat analysis dashboard

--- src/components/pages/AnalysisDashboard.vue
+++ src/components/pages/AnalysisDashboard.vue
@@ -133,6 +133,7 @@ import { Line as LineChartGenerator } from 'vue-chartjs/legacy'
 import zoomPlugin from 'chartjs-plugin-zoom';
 
 import ScalarPlot from '@/components/ui/ScalarPlot.vue'
+import SquatAnalysis from '@/components/ui/SquatAnalysis.vue'
 import LinearChart from '@/components/ui/LinearChart.vue'
 
 import {
@@ -162,6 +163,7 @@ export default {
     components: {
         Visualizer,
         ScalarPlot,
+        SquatAnalysis,
         LinearChart,
     },
     data() {
---
+++ src/components/ui/SquatAnalysis.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="squat-analysis-wrapper">
+    <div class="squat-type-header">Detected squat type</div>
+    <div class="squat-type-text">{{this.result.squat_type}}</div>
+    <div v-for="(row, row_idx) in sortedMetrics"
+         :key="row_idx"
+         :class="row.classes">
+      <div class="label-text">{{ row.label }}</div>
+      <div class="scalar-text">{{ row.value }} &plusmn; {{ row.std }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["trialID", 'timePosition', 'result', 'block'],
+  name: "squat-analysis",
+  computed: {
+    sortedMetrics() {
+      return Object.keys(this.result.metrics).sort().map((x) => this.result.metrics[x]);
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<style>
+.squat-type-header {
+  font-size: 16px;
+  text-align: center;
+  font-weight: bold;
+}
+
+.squat-type-text {
+  font-size: 28px;
+  text-align: center;
+  margin-bottom: 3rem;
+}
+
+.label-text {
+  font-size: 16px;
+  text-align: center;
+  font-weight: bold;
+}
+
+.scalar-text {
+  font-size: 32px;
+  text-align: center;
+  margin-bottom: 3rem;
+}
+
+.squat-analysis-wrapper {
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+}
+
+.squat-analysis {
+  width: 250px;
+  height: 100vh;
+  background: black;
+  color: white;
+  margin-bottom: 1rem;
+  overflow-y: auto;
+}
+
+</style>
