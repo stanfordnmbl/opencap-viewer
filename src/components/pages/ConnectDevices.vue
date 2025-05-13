02b0e3ad421daa25554d3547a015ab37e6933d89
Seeeeeyo <selimgilon@gmail.com>
2025-05-13 14:00:11 -0600

    Modified the view for ipad

--- src/components/pages/ConnectDevices.vue
+++ src/components/pages/ConnectDevices.vue
@@ -2,60 +2,60 @@
   <MainLayout
     column
     :step="1"
+    :fixedHeight="false"
     :rightDisabled="loading">
 
     <!-- Custom right section with two buttons -->
     <template v-slot:right>
-      <v-btn
-        v-if="hasMonoAccess"
-        class="mr-2"
-        :disabled="loading"
-        @click="skipToSession">
-        Next to monocular
-      </v-btn>
-      <v-btn
-        :disabled="loading"
-        @click="onNext">
-        Next
-      </v-btn>
+      <div class="d-none"><!-- Hiding default right buttons positioning --></div>
     </template>
 
-    <v-card class="flex-grow-1 d-flex flex-column justify-center">
-      <v-card-text class="d-flex flex-column align-center">
+    <div class="connect-devices-wrapper">
+      <v-card class="connect-devices-card">
+        <v-card-text class="d-flex flex-column align-center">
+          <div class="d-flex step-1 align-center flex-wrap">
+            <div class="d-flex flex-column flex-grow-1 justify-space-between my-1">
+              <h1 class="my-4">1. Open the OpenCap app on your phone</h1>
+              <h1 class="my-4">2. Scan the QR code</h1>
+              <h1 class="my-4">3. Mount your phone vertically or horizontally (unlock portrait orientation) on a tripod</h1>
+              <h1 class="my-4">4. Position the tripod and camera to capture the volume of interest</h1>
+              <h1 class="my-4">5. Repeat 1-4 for all phones you want to connect</h1>
+              <h1 class="my-4">6. Have the person practice the activity and verify that they are fully in view of at least 2 cameras</h1>
+            </div>
 
-        <div class="d-flex step-1 align-center flex-wrap">
-          <div class="d-flex flex-column flex-grow-1 justify-space-between my-1">
-            <h1 class="my-4">1. Open the OpenCap app on your phone</h1>
-            <h1 class="my-4">2. Scan the QR code</h1>
-            <h1 class="my-4">3. Mount your phone vertically or horizontally (unlock portrait orientation) on a tripod</h1>
-            <h1 class="my-4">4. Position the tripod and camera to capture the volume of interest</h1>
-            <h1 class="my-4">5. Repeat 1-4 for all phones you want to connect</h1>
-            <h1 class="my-4">6. Have the person practice the activity and verify that they are fully in view of at least 2 cameras</h1>
+            <div class="image-container qr-container d-flex align-center justify-center my-1">
+              <v-progress-circular
+                v-if="loading"
+                indeterminate 
+                color="grey" 
+                size="32" 
+                width="5"/>
+
+              <img
+                v-else
+                class="w-100 h-100"
+                :src="session.qrcode">
+            </div>
           </div>
-
-          <div class="image-container qr-container d-flex align-center justify-center my-1">
-            <v-progress-circular
-              v-if="loading"
-              indeterminate 
-              color="grey" 
-              size="32" 
-              width="5"/>
-
-            <img
-              v-else
-              class="w-100 h-100"
-              :src="session.qrcode">
-          </div>
-        </div>
-
-        <!--div class="cameras d-flex align-center justify-center mt-5">
-          <v-text-field
-            v-model="cameras"
-            label="Connected cameras"
-            class="cameras"/>
-        </div-->
-      </v-card-text>
-    </v-card>
+        </v-card-text>
+      </v-card>
+      
+      <!-- Custom navigation buttons positioned below the card -->
+      <div class="custom-navigation d-flex justify-end mt-4">
+        <v-btn
+          v-if="hasMonoAccess"
+          class="mr-2"
+          :disabled="loading"
+          @click="skipToSession">
+          Next to monocular
+        </v-btn>
+        <v-btn
+          :disabled="loading"
+          @click="onNext">
+          Next
+        </v-btn>
+      </div>
+    </div>
   </MainLayout>
 </template>
 
@@ -125,8 +125,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.connect-devices-wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+
+.connect-devices-card {
+  flex: 1;
+  overflow-y: auto;
+}
+
 .step-1 {
-  width:100%;
+  width: 100%;
 
   .qr-container {
     width: 200px;
@@ -137,5 +149,13 @@ export default {
 
 .cameras {
   width: 120px;
+}
+
+.custom-navigation {
+  width: 100%;
+  margin-top: 16px;
+  padding-bottom: 16px;
+  position: sticky;
+  bottom: 0;
 }
 </style>
