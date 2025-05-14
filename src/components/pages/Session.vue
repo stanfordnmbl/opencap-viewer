ad2c1c6bbc959c88034952ca12670e298cb65f41
Seeeeeyo <selimgilon@gmail.com>
2025-05-13 18:30:30 -0600

    increased floor size

--- src/components/pages/Session.vue
+++ src/components/pages/Session.vue
@@ -1317,7 +1317,7 @@
                   // show3d
                   // add the plane
                   {
-                    const planeSize = 5;
+                    const planeSize = 10;
   
                     const loader = new THREE.TextureLoader();
                     const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png');
