14441dbc8d154a5bcb7b78addf7e75da5ff66904
suhlrich <43877159+suhlrich@users.noreply.github.com>
2025-07-21 12:58:44 -0600

    deps

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
 module.exports = {
+  // Tell Vue CLI to transpile this dependency (it uses modern JS syntax like `??`)
+  transpileDependencies: [
+    'chartjs-plugin-zoom'
+  ],
+
+  // Extend webpack config to use vue-template-babel-compiler
   chainWebpack: config => {
     config.module
       .rule('vue')
       .use('vue-loader')
       .tap(options => {
-        options.compiler = require('vue-template-babel-compiler')
-        return options
-      })
+        options.compiler = require('vue-template-babel-compiler');
+        return options;
+      });
   }
-}
+};
